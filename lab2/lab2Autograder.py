f302dbdb2664d2df5a824b4ae87902ec7db2c113
Matej Grcic <m.j.grcic7@gmail.com>
2018-04-13 14:34:39 +0200

    autograder fixed

--- lab2/lab2Autograder.py
+++ lab2/lab2Autograder.py
@@ -1,14 +1,15 @@
 import os
 
 correct = 0
+total_played = 0
 incorrectMazes = []
 for i in range(0,30):
-    command = "pacman.py -l lay{} -p PacardAgent -a fn=logicBasedSearch -g WumpusGhost".format(i)
+    command = "pacman.py -l lay{} -p PacardAgent -a fn=logicBasedSearch -g WumpusGhost -q".format(i)
     x = os.system(command)
     if x == 0:
         correct = correct + 1
     else:
         incorrectMazes.append('lay{}'.format(i))
-
-print "{}/30 succeded".format(correct)
+    total_played = total_played + 1
+print "{}/{} succeded".format(correct, total_played)
 print incorrectMazes
