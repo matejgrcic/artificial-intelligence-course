75cdd22e3665836850e5ee78d6f18f1410024b2e
Matej Grcic <m.j.grcic7@gmail.com>
2018-04-15 11:28:14 +0200

    testing done.

--- lab2/testsForLogic.py
+++ lab2/testsForLogic.py
@@ -1,16 +1,16 @@
 import logic
 import os
+import sys
+import time
 
 correct = 0
 
-"""
-Rjesava jedan test i vraca string zapis rjesenja i stvarnog rjesenja.
-"""
+
 def resolveTest(path):
     global correct
     f = open(path)
     premises = set()
-    
+
     solution = f.readline().strip()
     for line in f:
         literals = set()
@@ -18,26 +18,29 @@ def resolveTest(path):
         for lit in lineSplit:
             if lit.startswith('-'):
                 literals.add(logic.Literal(lit[1:], (0, 0), True))
-	    else:
-		literals.add(logic.Literal(lit, (0, 0), False))
+            else:
+                literals.add(logic.Literal(lit, (0, 0), False))
         last = logic.Clause(literals)
         premises.add(last)
     goal = last
     premises.remove(last)
     resolution = logic.resolution(premises, goal)
     if str(resolution) == solution:
-	correct += 1
-    return str(resolution) + ' | Correct solution: ' + solution
+        correct += 1
+    return 'Calculated: '+ str(resolution) + ' Correct solution: ' + solution
 
 
 if __name__ == '__main__':
     total = 0
+    cnt = 0
     for subdir, dirs, files in os.walk('./test/'):
-	for file in files:
-	    filename = subdir + os.sep + file	    
-	    print filename
-	    print resolveTest(filename)
-	    print
-	    total += 1
-    print
-    print 'Test results: ' + str(correct) + '/' + str(total)
+        for file in files:
+            filename = subdir + os.sep + file
+            cnt = cnt + 1
+            print 'test{}:'.format(cnt)
+            print resolveTest(filename)
+            print
+            total += 1
+    time.sleep(0.5)
+    print >> sys.stderr, '+++++++++++++++++++++++++++++++++++++++++'
+    print >> sys.stderr, 'Total: ' + str(correct) + '/' + str(total) + ' passed!'
